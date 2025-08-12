d9a02b8368609bfd749d3a001831fb175da2986f
albmartmart <alberto.martin@htecgroup.com>
2025-08-12 13:31:58 +0200

    NOSI-6 Added dummy package and rules.
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>

---
+++ rules/hello-sonic-pkg.mk
@@ -0,0 +1,7 @@
+# Python wheel package (no global inclusion here)
+HELLO_PY3 = hello-sonic-pkg-0.1.0-py3-none-any.whl
+$(HELLO_PY3)_SRC_PATH       = $(SRC_PATH)/hello-sonic-pkg
+$(HELLO_PY3)_PYTHON_VERSION = 3
+$(HELLO_PY3)_TEST           = n
+
+SONIC_PYTHON_WHEELS += $(HELLO_PY3)
