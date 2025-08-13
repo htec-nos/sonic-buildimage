05ab6ba2bca82157ffb6737ebebf2a6798fa1985
albmartmart <alberto.martin@htecgroup.com>
2025-08-13 13:36:33 +0200

    NOSI-6: Changed dummy python package. The changes aims to fit the SONiC legacy query (python setup.py --name). Now the dummy package has both pyproject.toml and setup.cfg.
    * pyproject.toml: include the build backend
    * setup.cfg: include metadata of the package and project info
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>

--- rules/hello-sonic-pkg.mk
+++ rules/hello-sonic-pkg.mk
@@ -1,5 +1,5 @@
 # Python wheel package (no global inclusion here)
-HELLO_PY3 = hello-sonic-pkg-0.1.0-py3-none-any.whl
+HELLO_PY3 = hello_sonic_pkg-0.1.0-py3-none-any.whl
 $(HELLO_PY3)_SRC_PATH       = $(SRC_PATH)/hello-sonic-pkg
 $(HELLO_PY3)_PYTHON_VERSION = 3
 $(HELLO_PY3)_TEST           = n
