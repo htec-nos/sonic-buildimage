b86b0936542e97e9869445f3b4d07ad0d941d87b
albmartmart <alberto.martin@htecgroup.com>
2025-08-28 12:46:14 +0200

    NOSI-24: Fixed building bug.
    * control: added necessary blank space
    * rules: simplified
    * setup.py: added find_package to setup function
    
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>

--- src/hello-sonic-pkg/setup.py
+++ src/hello-sonic-pkg/setup.py
@@ -1,4 +1,6 @@
-import setuptools
+from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setup(
+        packages=find_packages(include=["hello_sonic_pkg", "hello_sonic_pkg.*"])
+    )
