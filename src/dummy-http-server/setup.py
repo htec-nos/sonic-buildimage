46dbe3b86f1a9628f0acb04e9a5c82d7be0953f6
albmartmart <alberto.martin@htecgroup.com>
2025-09-01 10:27:36 +0200

    NOSI-25: Change name to dummy HTTP server.
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>

--- src/dummy-http-server/setup.py
+++ src/dummy-http-server/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 if __name__ == "__main__":
     setup(
-        packages=find_packages(include=["hello_sonic_pkg", "hello_sonic_pkg.*"])
+        packages=find_packages(include=["dummy_http", "dummy_http.*"])
     )
