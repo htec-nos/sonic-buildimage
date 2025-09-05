1df3f4e3bbec669ec1924f87a2b88492a2712a92
albmartmart <alberto.martin@htecgroup.com>
2025-09-05 16:07:03 +0200

    NOSI-54: Added basic logging to dummy-http-server.
    Signed-off-by: albmartmart <alberto.martin@htecgroup.com>

--- src/dummy-http-server/dummy_http/__main__.py
+++ src/dummy-http-server/dummy_http/__main__.py
@@ -1,10 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from syslog import syslog, LOG_INFO
 
 class DummyHandler(BaseHTTPRequestHandler):
+    num_get_requests = 0
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello from dummy SONiC service!\n")
+        DummyHandler.num_get_requests += 1
+        syslog(LOG_INFO, f"Hello syslog! Handled GET request number {DummyHandler.num_get_requests}")
+        print("Hello stdout! This comes from a normal python print statement.")
 
 def main():
     server = HTTPServer(("0.0.0.0", 8080), DummyHandler)
